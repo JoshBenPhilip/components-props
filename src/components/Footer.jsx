db9289e6e17c3c5a2426e7c3b65a7334391cf6bf
JoshBenPhilip <joshben4c@gmail.com>
2022-05-16 16:23:33 -0400

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Footer() {
+    return(
+        <footer style={{ width: '100%', backgroundColor: 'wheat'
+        }}>
+            <p>
+                &copy; Henlo, 2022.
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
